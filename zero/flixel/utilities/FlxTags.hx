0815da7c3df8e0d9f47af2078499b13db03d26b5
01010111 <will.blanton.804@gmail.com>
2020-09-02 12:08:34 -0400

    add FlxTags

---
+++ zero/flixel/utilities/FlxTags.hx
@@ -0,0 +1,56 @@
+package zero.flixel.utilities;
+
+import flixel.FlxObject;
+
+using zero.extensions.ArrayExt;
+
+/**
+	A Utility for adding tags to objects, checking to see if an object has a tag, and getting all objects with a specific tag.
+	Usage:
+	```
+	// in your imports header:
+	using zero.flixel.utilities.FlxTags;
+
+	// elsewhere...
+	var sprite1 = new FlxObject();
+	var sprite2 = new FlxSprite();
+
+	sprite1.add_tag('object');
+	sprite2.add_tags(['object', 'pickup']);
+
+	sprite2.has_tag('pickup'); // true
+	FlxTags.get_objects('object'); // [sprite1, sprite2]
+	```
+**/
+class FlxTags {
+	
+	static var map:Map<String, Array<FlxObject>> = [];
+
+    public static function add_tag(object:FlxObject, tag:String) {
+		if (!map.exists(tag)) map.set(tag, []);
+		map[tag].push(object);
+	}
+
+	public static function add_tags(object:FlxObject, tags:Array<String>) {
+		for (tag in tags) add_tag(object, tag);
+	}
+
+	public static function remove_tag(object:FlxObject, tag:String) {
+		return map.exists(tag) && map[tag].remove(object);
+	}
+
+	public static function has_tag(object:FlxObject, tag:String):Bool {
+		return map.exists(tag) && map[tag].contains(object);
+	}	
+
+	public static function remove_all_tags(object:FlxObject) {
+		for (array in map) array.remove(object);
+	}
+
+	public static function get_objects(tag:String):Array<FlxObject> {
+		if (!map.exists(tag)) return [];
+		for (object in map[tag]) if (object == null) map[tag].remove(object);
+		return map[tag].copy();
+	}
+
+}
