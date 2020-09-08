935abd3980ac550dd4fe791c1cd9fedbd6693385
01010111 <will.blanton.804@gmail.com>
2020-09-08 14:49:44 -0400

    add get_2D_array()

--- zero/flixel/extensions/FlxTilemapExt.hx
+++ zero/flixel/extensions/FlxTilemapExt.hx
@@ -11,32 +11,27 @@ class FlxTilemapExt
 
 	/**
 	 * Returns the tile index at a given point
-	 * @param t	tilemap
-	 * @param p point
-	 * @return Int
 	 */
 	public static inline function get_index_from_point(t:FlxTilemap, p:FlxPoint):Int return t.getTile((p.x / t.get_tile_width()).floor(), (p.y / t.get_tile_height()).floor());
 
 	/**
 	 * Returns a tile's allowCollisions value at a given point
-	 * @param t	tilemap
-	 * @param p	point
-	 * @return Int
 	 */
 	public static inline function get_collisions_from_point(t:FlxTilemap, p:FlxPoint):Int return t.getTileCollisions(t.get_index_from_point(p));
 
 	/**
 	 * Returns the tile width of a tilemap
-	 * @param t tilemap
-	 * @return return
 	 */
 	public static inline function get_tile_width(t:FlxTilemap):Float return t.width / t.widthInTiles;
 
 	/**
 	 * Returns the tile height of a tilemap
-	 * @param t tilemap
-	 * @return return
 	 */
 	public static inline function get_tile_height(t:FlxTilemap):Float return t.height / t.heightInTiles;
+
+	/**
+	 * Returns a 2D array of tile indexes
+	**/
+	public static inline function get_2D_array(t:FlxTilemap):Array<Array<Int>> return [for (j in 0...t.heightInTiles) [ for (i in 0...t.widthInTiles) t.getTileByIndex(i + j * t.widthInTiles)]];
 
 }
